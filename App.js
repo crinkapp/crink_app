4ec60058569f754c195a36b1729271d3db12ebc4
Saïdou Coulibaly <saidou.coulibaly@outlook.fr>
2020-06-25 18:54:47 +0200

    add signup content

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import Regform from './app/components/Regform';
+
+export default class App extends React.Component {
+  render() {
+      return (
+        <View style={styles.container}>
+          <Regform />
+        </View>
+      );
+    }
+  }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor : '#B96C55',
+    paddingLeft : 60,
+    paddingRight : 60,
   },
 });
---
+++ app/components/Regform.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, ImageBackground } from 'react-native';
+
+export default function App() {
+  return (
+
+    <View style={styles.regform}>
+
+    <Image style={styles.logo}
+            source={require('../../assets/icon/crink-logo-white.png')} />
+       
+            <Text style={styles.header}>Inscription</Text>
+
+            <TextInput style={styles.textinput} placeholder="Votre Nom d'utilisateur " 
+            placeholderTextColor="#ffffff"/>
+
+            <TextInput style={styles.textinput} placeholder="Email" 
+            placeholderTextColor="#ffffff"/>
+
+            <TextInput style={styles.textinput} placeholder="Mot de Passe" 
+            placeholderTextColor="#ffffff"/>
+
+            <TextInput style={styles.textinput} placeholder="Confirmation de Mot de passe" 
+            placeholderTextColor="#ffffff"/>
+
+            <TouchableOpacity style={styles.button}> 
+                <Text style={styles.btntext}>S'inscrire </Text>
+            </TouchableOpacity>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  regform: {
+    alignSelf : 'stretch',
+    alignItems: 'center',
+  },
+    header: {
+    fontSize: 24,
+    color: '#fff',
+    paddingBottom : 10,
+    marginBottom : 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  textinput: {
+    alignSelf : 'stretch',
+    height :  40,
+    marginBottom :30,
+    color: "#ffffff",
+    borderColor: '#f8f8f8',
+    borderWidth: 1,
+    alignItems: 'center',
+    paddingLeft: 5,
+  },
+
+  button: {
+      alignSelf:'stretch',
+      alignItems: 'center',
+      padding : 20,
+      backgroundColor: '#ffffff',
+      marginTop:3,
+  },
+  btntext:{
+      color:'#000000',
+      fontWeight:'bold',
+  },
+
+  logo:{
+    width : 160,
+    height: 50,
+    marginBottom : 20,
+  }
+});
