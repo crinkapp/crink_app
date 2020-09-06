593a8d3e872cb2453f74e68f7aaf6e9f9a274e66
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-06 22:57:11 +0200

    optimize style

--- src/styles.js
+++ src/styles.js
@@ -3,29 +3,23 @@ import { StyleSheet } from "react-native";
 const mainColor = "#B96C55";
 const darkColor = "#3A444C";
 const lightColor = "#FAECE3";
+const redColor = "#D55E5E";
 
 export default StyleSheet.create({
   // Sign In & Sign Up Screens
   signScreen: {
     flex: 1,
     flexDirection: "column",
+    backgroundColor: lightColor,
+    paddingVertical: 30,
+    paddingHorizontal: 20,
   },
   signInputText: {
-    width: 300,
+    width: "100%",
     padding: 14,
     borderWidth: 1,
-    borderColor: lightColor,
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "300",
-    marginVertical: 10,
-  },
-  signInputTextError: {
-    width: 300,
-    padding: 14,
-    borderWidth: 1,
-    borderColor: "#DD655C",
-    color: "#fff",
+    borderRadius: 6,
+    color: darkColor,
     fontSize: 16,
     fontWeight: "300",
     marginVertical: 10,
@@ -33,8 +27,9 @@ export default StyleSheet.create({
   signBtn: {
     marginTop: 40,
     paddingVertical: 6,
-    width: 300,
-    backgroundColor: "rgba(0,0,0,0.5)",
+    width: "100%",
+    backgroundColor: darkColor,
+    borderRadius: 6,
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
