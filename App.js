feb3a06f873f22460d42b9b70e408497621bafda
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-08 15:18:55 +0200

    clean up

--- App.js
+++ App.js
@@ -2,31 +2,39 @@ import "react-native-gesture-handler";
 import React, { useState } from "react";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
-import Navigation from "./components/navigation"
+import Navigation from "./src/components/navigation"
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    "montserrat-black": require("./assets/fonts/Montserrat-Black.ttf"),
-    "montserrat-medium": require("./assets/fonts/Montserrat-Medium.ttf"),
-    "montserrat-regular": require("./assets/fonts/Montserrat-Regular.ttf"),
-    "montserrat-light": require("./assets/fonts/Montserrat-Light.ttf"),
-  });
-};
+// const fetchFonts = () => {
+//   return Font.loadAsync({
+//     "montserrat-black": require("./assets/fonts/Montserrat-Black.ttf"),
+//     "montserrat-medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+//     "montserrat-regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+//     "montserrat-light": require("./assets/fonts/Montserrat-Light.ttf"),
+//   });
+// };
 
-export function App() {
-  const [dataLoaded, setDataLoaded] = useState(false);
-
-  // Family font fetch
-  if (!dataLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setDataLoaded(true)}
-      />
-    );
+export class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
   }
 
-  return <Navigation></Navigation>
+  // const [dataLoaded, setDataLoaded] = useState(false);
+
+  // // Family font fetch
+  // if (!dataLoaded) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={fetchFonts}
+  //       onFinish={() => setDataLoaded(true)}
+  //     />
+  //   );
+  // }
+  render() {
+    return <Navigation></Navigation>
+  }
 }
 
 export default App;
