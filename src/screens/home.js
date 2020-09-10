de44bcce7d96d6f34c09e81f77b1875926d77919
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-10 23:49:28 +0200

    get publications from api in home page

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,47 +1,84 @@
 import React, { Component } from "react";
-import { View, Image, Text, StyleSheet } from "react-native";
+import { View, Image, Text, StyleSheet, ScrollView } from "react-native";
 import PublicationPreview from "../components/publication-preview";
+import axios from "axios";
+import { API_URL, S3_URL } from "react-native-dotenv";
+import { LongPressGestureHandler } from "react-native-gesture-handler";
+import { log } from "react-native-reanimated";
 
-const image1 = require("../../assets/img/pic1.jpeg");
-const title1 = "Comment peigner vos cheveux bouclés ?"
-const tags1 = ["bouclés", "afro", "peigne", "soins"];
-const likes1 = 723;
-const comments1 = 32;
-const username1 = "Saïdou";
-const userIcon1 = require("../../assets/img/usericon.jpeg");
+// const image1 =
+//   "https://images.unsplash.com/photo-1480796927426-f609979314bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80";
+// const title1 = "Comment peigner vos cheveux bouclés ?";
+// const tags1 = ["bouclés", "afro", "peigne", "soins"];
+// const likes1 = 723;
+// const comments1 = 32;
+// const username1 = "Saïdou";
+// const userIcon1 =
+//   "https://www.envolgym.org/envolgym_data/upload/images/SITE/EQUIPE/anon.png";
 
-const image2 = require("../../assets/img/savane.jpeg");
-const title2 = "Comment voir des girafes dans la savane ?"
-const tags2 = ["girafe", "savane", "animal"];
-const likes2 = 413;
-const comments2 = 19;
-const username2 = "Aïssatou";
-const userIcon2 = require("../../assets/img/pic2.jpeg");
+// const image2 =
+//   "https://images.unsplash.com/photo-1528360983277-13d401cdc186?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80";
+// const title2 = "Comment voir des girafes dans la savane ?";
+// const tags2 = ["girafe", "savane", "animal"];
+// const likes2 = "63k";
+// const comments2 = "2k";
+// const username2 = "Crink";
+// const userIcon2 =
+//   "file:///Users/eudreybandelaria/Documents/ipssi/projet-annuel/icon/fav-icon.png";
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      publications: [],
+    };
+  }
+  // pathImg: "",
+  // title: "",
+  // // tags: "",
+  // // likes: "",
+  // // comments: "",
+  // username: "Eudrey",
+  // userIcon:
+  //   "https://www.envolgym.org/envolgym_data/upload/images/SITE/EQUIPE/anon.png",
+
+  componentDidMount() {
+    this.getAllPublications();
+  }
+
+  getAllPublications = async () => {
+    return await axios
+      .get(`${API_URL}/all-publications`)
+      .then((res) => {
+        this.setState({
+          publications: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
-      <View style={styles.screen}>
-        <PublicationPreview
-          image={image1}
-          title={title1}
-          tags={tags1}
-          likes={likes1}
-          comments={comments1}
-          username={username1}
-          userIcon={userIcon1}
-        ></PublicationPreview>
-
-        <PublicationPreview
-          image={image2}
-          title={title2}
-          tags={tags2}
-          likes={likes2}
-          comments={comments2}
-          username={username2}
-          userIcon={userIcon2}
-        ></PublicationPreview>
-      </View>
+      <ScrollView>
+        <View style={styles.screen}>
+          {this.state.publications.length > 0 ? (
+            this.state.publications.map((prop, key) => {
+              return (
+                <PublicationPreview
+                  onPress={() => console.log('testing')}
+                  key={key}
+                  previewImg={`${S3_URL}/${prop.path_media_publication}`}
+                  title={prop.title_publication}
+                  username="eudrey"
+                  userIcon="https://www.envolgym.org/envolgym_data/upload/images/SITE/EQUIPE/anon.png"
+                ></PublicationPreview>
+              );
+            })
+          ) : (
+            <Text style={styles.text}>Il n'y pas de publications…</Text>
+          )}
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -57,6 +94,6 @@ const styles = StyleSheet.create({
     color: "#3A444C",
     fontWeight: "300",
     paddingBottom: 30,
-    fontSize: 28,
+    fontSize: 22,
   },
 });
