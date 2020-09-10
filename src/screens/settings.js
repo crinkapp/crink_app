3ecd72ad7c92788699071e4c9a21b032d014ccc4
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-10 23:51:33 +0200

    added sign out function in settings

--- src/screens/settings.js
+++ src/screens/settings.js
@@ -1,11 +1,44 @@
 import React, { Component } from "react";
-import { View, Image, Text, StyleSheet } from "react-native";
+import { View, Image, Text, StyleSheet, Button } from "react-native";
+import axios from "axios";
+import { API_URL } from "react-native-dotenv";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default class Settings extends React.Component {
+  logout = () => {
+    return axios.delete(`${API_URL}/user`);
+  };
+
+  _onLogout = () => {
+    this.logout()
+      .then((res) => console.log(`Disconnect : ${JSON.stringify(res.data)}`))
+      .catch((err) => console.log(`Error : ${err.response} test`));
+  };
+
+  getUser = () => {
+    return axios
+      .get(`${API_URL}/user`)
+      .then((res) => {
+        console.log(`error server : ${err.response}`);
+      })
+      .catch((err) => {
+        // If incorrect email or password
+        if (err.request) {
+          console.log(`error server : ${err.response}`);
+        }
+      });
+  };
+
   render() {
     return (
       <View style={styles.screen}>
         <Text style={styles.text}>Écran de paramètre</Text>
+        <TouchableOpacity onPress={this._onLogout}>
+          <Button
+            title="Se déconecter"
+            color="#D55E5E"
+          ></Button>
+        </TouchableOpacity>
       </View>
     );
   }
