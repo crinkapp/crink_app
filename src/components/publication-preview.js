8708c1a3c94e5ea832568b760b024fbb7c4dba09
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-10 23:47:03 +0200

    added first version of publication preview

--- src/components/publication-preview.js
+++ src/components/publication-preview.js
@@ -5,37 +5,37 @@ import Icon from "react-native-vector-icons/FontAwesome";
 const publicationPreview = (props) => {
   return (
     <View style={style.preview}>
-      <Image source={props.image} style={style.previewImg}></Image>
-      <View style={style.infos}>
+      <Image source={{uri: props.previewImg}} style={style.previewImg}></Image>
+      <View style={[style.infos, {alignItems: "flex-start"}]}>
         <Text style={style.title}>{props.title}</Text>
-        <Icon name="share" size={16} color="#3A444C" />
+        <Icon name="share" style={{marginLeft: "auto"}} size={16} color="#3A444C" />
       </View>
       <View style={style.infos}>
         <View style={style.tags}>
-          {props.tags.map((prop, key) => {
+          {props.tags ? props.tag.map((prop, key) => {
             return (
               <Text style={style.tag} key={key}>
                 #{prop}
               </Text>
             );
-          })}
+          }) : <Text>…</Text> }
         </View>
         <Text style={style.date}>il y a 1 jour</Text>
       </View>
       <View style={style.infos}>
         <View style={style.tags}>
           <View style={style.likeComments}>
-            <Icon name="heart" size={16} color="#D55E5E" />
-            <Text style={style.likeComment}>{props.likes}</Text>
+            <Icon name="heart" size={16} color="#CFCECE" />
+            <Text style={style.likeComment}>{props.likes ? props.likes : <Text>…</Text>}</Text>
           </View>
           <View style={style.likeComments}>
             <Icon name="comment" size={16} color="#CFCECE" />
-            <Text style={style.likeComment}>{props.comments}</Text>
+            <Text style={style.likeComment}>{props.comments ? props.comments : <Text>…</Text>}</Text>
           </View>
         </View>
         <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Text style={style.username}>par {props.username}</Text>
-          <Image style={style.userIcon} source={props.userIcon}></Image>
+          <Text style={style.username}>par {props.username ? props.username : <Text>…</Text>}</Text>
+          <Image style={style.userIcon} source={{uri: props.userIcon ? props.userIcon : <Text>…</Text>}}></Image>
         </View>
       </View>
     </View>
@@ -54,21 +54,21 @@ const style = StyleSheet.create({
     paddingBottom: 15,
   },
   previewImg: {
-    backgroundColor: "lightblue",
     height: 150,
     width: "100%",
-    resizeMode: "center",
+    resizeMode: "cover",
   },
   infos: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    marginHorizontal: 15,
+    paddingHorizontal: 15,
     marginTop: 10,
   },
   title: {
     fontWeight: "500",
     color: "#3A444C",
+    width: "80%"
   },
   tags: {
     flexDirection: "row",
