a4288043d9809b7dcd04a65de52878b6919d5812
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-11 22:14:42 +0200

    fixe navigation

--- App.js
+++ App.js
@@ -1,36 +1,12 @@
 import "react-native-gesture-handler";
 import React, { useState, useEffect } from "react";
-import * as Font from "expo-font";
 import { AppLoading } from "expo";
 import Navigation from "./src/components/navigation";
 import { AsyncStorage } from "react-native";
 import { API_URL } from "react-native-dotenv";
 import axios from "axios";
 
-// import Navigation from "./src/components/navigation";
-
-// const fetchFonts = () => {
-//   return Font.loadAsync({
-//     "montserrat-black": require("./assets/fonts/Montserrat-Black.ttf"),
-//     "montserrat-medium": require("./assets/fonts/Montserrat-Medium.ttf"),
-//     "montserrat-regular": require("./assets/fonts/Montserrat-Regular.ttf"),
-//     "montserrat-light": require("./assets/fonts/Montserrat-Light.ttf"),
-//   });
-// };
-
 const App = () => {
-  // const [dataLoaded, setDataLoaded] = useState(false);
-
-  // // Family font fetch
-  // if (!dataLoaded) {
-  //   return (
-  //     <AppLoading
-  //       startAsync={fetchFonts}
-  //       onFinish={() => setDataLoaded(true)}
-  //     />
-  //   );
-  // }
-
   const [user, setUser] = useState({});
 
   const getUser = () => {
