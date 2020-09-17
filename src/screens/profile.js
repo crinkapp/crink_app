690e2bead595330110db25951acca573f3c202bd
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-17 18:20:48 +0200

    added message btn

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -13,22 +13,7 @@ const Profile = (prop) => {
   );
   return (
     <ScrollView>
-      <View
-        style={[
-          globalStyle.appScreen,
-          {
-            borderBottomLeftRadius: 30,
-            borderBottomRightRadius: 30,
-            shadowOffset: {
-              width: 0, // x
-              height: 6, // y
-            },
-            shadowRadius: 5,
-            shadowOpacity: 0.1,
-            shadowColor: "#3A444C",
-          },
-        ]}
-      >
+      <View style={[globalStyle.appScreen, styles.header]}>
         <Image
           source={{ uri: prop.route.params.iconPath }}
           style={styles.image}
@@ -39,17 +24,17 @@ const Profile = (prop) => {
           <Text style={styles.username}>{user.username_user}</Text>
           <Icon name="certificate" size={16} color="#379EE5" />
         </View>
-        <TouchableOpacity style={styles.subscribeBtn}>
-          <Text style={styles.subscribeLabel}>S'abonner</Text>
-        </TouchableOpacity>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            padding: 20,
-            width: "100%",
-          }}
-        >
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity style={[styles.subscribeBtn, styles.btn]}>
+            <Text style={styles.subscribeLabel}>S'abonner</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[styles.messageBtn, styles.btn]}>
+            <Icon name="paper-plane" size={12} color="#fff" solid />
+            <Text style={styles.messageLabel}>Message</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.statsView}>
           <View style={styles.stats}>
             <Text style={styles.numbers}>12</Text>
             <Text style={styles.statsLabel}>ARTICLES</Text>
@@ -81,6 +66,17 @@ const Profile = (prop) => {
 };
 
 const styles = StyleSheet.create({
+  header: {
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    shadowOffset: {
+      width: 0, // x
+      height: 6, // y
+    },
+    shadowRadius: 5,
+    shadowOpacity: 0.1,
+    shadowColor: "#3A444C",
+  },
   image: {
     height: 80,
     width: 80,
@@ -105,6 +101,12 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     color: "#3A444C",
   },
+  statsView: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    padding: 20,
+    width: "100%",
+  },
   statsLabel: {
     fontSize: 9,
     color: "#ABABAB",
@@ -113,19 +115,40 @@ const styles = StyleSheet.create({
     borderLeftWidth: 0.6,
     borderLeftColor: "#ABABAB",
   },
-  subscribeBtn: {
-    marginTop: 20,
+  btn: {
+    marginTop: 14,
     marginBottom: 8,
-    backgroundColor: "#fff",
     borderWidth: 1,
     borderColor: "#3A444C",
-    borderRadius: 10,
+    borderRadius: 6,
+  },
+  subscribeBtn: {
+    paddingHorizontal: 4,
+  },
+  messageBtn: {
+    marginBottom: 8,
+    marginLeft: 10,
+    backgroundColor: "#3A444C",
+    borderRadius: 6,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: "auto",
   },
   subscribeLabel: {
-    fontSize: 13,
+    fontSize: 12,
     paddingHorizontal: 10,
     paddingVertical: 6,
     color: "#3A444C",
+    textAlign: "center",
+  },
+  messageLabel: {
+    fontSize: 12,
+    paddingVertical: 6,
+    color: "#fff",
+    textAlign: "center",
+    marginLeft: 6,
   },
 });
 
