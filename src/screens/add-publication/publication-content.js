bf135837a6fdfb584aab4c65ccd990a0a2bd0b25
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-22 18:46:59 +0200

    create first version of adding publication via app

--- src/screens/add-publication/publication-content.js
+++ src/screens/add-publication/publication-content.js
@@ -1,33 +1,75 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import globalStyle from "../../styles";
+import axios from "axios";
+import { API_URL } from "react-native-dotenv";
 
 const ContentPublication = (props) => {
   const [publication, setPublication] = useState(
     props.route.params.publication
   );
+  const [content, setContent] = useState("");
+
+  const addPublication = async (newPublication) => {
+    return await axios.post(`${API_URL}/add-publication`, newPublication);
+  };
+
+  const onSubmit = () => {
+    const newPublication = {
+      title: publication.title,
+      hashtags: publication.hashtags,
+      content,
+    };
+    addPublication(newPublication)
+      .then((success) => {
+        console.log(success);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <ScrollView backgroundColor="#fff">
       <View style={globalStyle.addPublicationScreen}>
         <Text style={{ color: "#3A444C", fontWeight: "300", marginBottom: 10 }}>
-          Titre : <Text style={{fontStyle: "italic"}}>{publication.title}</Text>
+          Titre : <Text style={{ fontWeight: "500" }}>{publication.title}</Text>
         </Text>
-        <View style={{flexDirection: "row"}}>
+        <View style={{ flexDirection: "row" }}>
           <Text style={{ color: "#3A444C", fontWeight: "300", marginRight: 8 }}>
             Tags :
           </Text>
           {publication.nameTags.map((prop, key) => {
             return (
-              <Text key={key} style={{ color: "#3A444C", fontWeight: "300", marginRight: 8, fontStyle: "italic" }}>
+              <Text
+                key={key}
+                style={{
+                  color: "#B96C55",
+                  fontWeight: "500",
+                  marginRight: 8,
+                }}
+              >
                 #{prop}
               </Text>
             );
           })}
         </View>
-        <Text style={styles.title}>
-          Entres le contenu de ta publication
-        </Text>
+        <Text style={styles.title}>Entres le contenu de ta publication</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => setContent(text)}
+          value={content}
+          placeholder="Commences à écrire…"
+          multiline={true}
+        />
+        <TouchableOpacity style={styles.submitBtn} onPress={() => onSubmit()}>
+          <Text style={styles.submitLabel}>Terminer et voir l'aperçu</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
@@ -41,5 +83,25 @@ const styles = StyleSheet.create({
     fontSize: 22,
     color: "#3A444C",
     fontWeight: "600",
+  },
+  textInput: {
+    width: "100%",
+    paddingVertical: 6,
+    marginTop: 20,
+    borderRadius: 4,
+    fontSize: 16,
+  },
+  submitBtn: {
+    alignSelf: "center",
+    marginTop: 100,
+    backgroundColor: "#5DAF79",
+    width: "100%",
+    borderRadius: 8,
+  },
+  submitLabel: {
+    color: "#fff",
+    padding: 10,
+    textAlign: "center",
+    fontSize: 16,
   },
 });
